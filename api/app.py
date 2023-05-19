235eca72373e80206d6476b2663f753782ae47c1
Omer Leibo <leibo.omer13@gmail.com>
2023-05-19 16:50:30 +0300

    modified crud for user, added package to pipfile and crud tests

--- api/app.py
+++ api/app.py
@@ -8,6 +8,7 @@ db = SQLAlchemy(app)
 
 from DB.models import User
 
+
 with app.app_context():
     db.create_all()
 
@@ -17,47 +18,60 @@ def hello():
     return "Hello World!?!?!?!?"
 
 
-# add user
+# add user to DB
 @app.route("/users", methods=['POST'])
 def create_user():
     try:
         data = request.get_json()
         new_user = User(id=data['id'], first_name=data['first_name'],
                         last_name=data['last_name'], password=data['password'],
-                        email=data['email'])
+                        email=data['email'].lower())
         db.session.add(new_user)
         db.session.commit()
-        return make_response(jsonify({'message': 'user created!'}), 201)
+        return make_response(jsonify({'message': new_user.json()}), 201)
     except Exception as e:
+        db.session.rollback()
+        error_message = str(e.orig)
+        if 'duplicate key value violates unique constraint "users_pkey"' in error_message:
+            return make_response(jsonify({'message': 'Error! ID already exists'}), 400)
+        elif 'duplicate key value violates unique constraint "users_email_key"' in error_message:
+            return make_response(jsonify({'message': 'Error! Email already exists'}), 400)
         return make_response(jsonify({'message': 'ERROR!'}), 500)
     
 
-#get all users
-@app.route("/users", methods=['GET'])
-def get_users():
+# get user by id from DB
+@app.route("/users/<int:id>", methods=['GET'])
+def get_user(id):
     try:
-        users = User.query.all()
-        return make_response(jsonify({'users': [user.json() for user in users]}),200)
+        user = User.query.filter_by(id=id).first()
+        return make_response(jsonify({'user': user.json()}), 200)
     except Exception as e:
-        return make_response(jsonify({'message': 'ERROR!'}), 500)
+        return make_response(jsonify({'message': 'ERROR! user not found'}), 400)
 
 
-#update user
+# update user details
 @app.route("/users/<int:id>", methods=['PUT'])
 def update_user(id):
     try:
         user = User.query.filter_by(id=id).first()
         if user:
             data = request.get_json()
+            user.id = data['id']
             user.first_name = data['first_name']
             user.last_name = data['last_name']
             user.password = data['password']
-            user.email = data['email']
+            user.email = data['email'].lower()
             db.session.commit()
-            return make_response(jsonify({'message': 'user updated!'}), 200)
-        return make_response(jsonify({'message': 'ERROR! user id not found'}), 500)
+            return make_response(jsonify({'message': user.json()}), 200)
+        return make_response(jsonify({'message': 'ERROR! user id not found'}), 400)
     except Exception as e:
-        return make_response(jsonify({'message', 'ERROR!'}), 500)
+        db.session.rollback()
+        error_message = str(e.orig)
+        if 'duplicate key value violates unique constraint "users_pkey"' in error_message:
+            return make_response(jsonify({'message': 'Error! ID already exists'}), 400)
+        elif 'duplicate key value violates unique constraint "users_email_key"' in error_message:
+            return make_response(jsonify({'message': 'Error! Email already exists'}), 400)
+        return make_response(jsonify({'message': 'ERROR!'}), 400)
     
 #delete user
 @app.route("/users/<int:id>", methods=['DELETE'])
@@ -67,7 +81,7 @@ def delete_user(id):
         if user:
             db.session.delete(user)
             db.session.commit()
-            return make_response(jsonify({'message':'user deleted!'}), 200)
-        return make_response(jsonify({'message':'ERROR! user not found'}), 500)
+            return make_response(jsonify({'message': user.json()}), 204)
+        return make_response(jsonify({'message':'ERROR! user not found'}), 400)
     except Exception as e:
-        return make_response(jsonify({'message':'ERROR!'}), 500)
+        return make_response(jsonify({'message':'ERROR!'}), 400)
---
+++ api/tests/test_crud.py
@@ -0,0 +1,92 @@
+import pytest
+from app import app, db
+from DB.models import User
+
+@pytest.fixture
+def test_client():
+    flask_app = app
+    flask_app.config["TESTING"] = True
+    
+    with flask_app.test_client() as testing_client:
+        with flask_app.app_context():
+            yield testing_client
+            # after test finished delete remaining user
+            user = User.query.filter_by(id=11).first()
+            if user:
+                db.session.delete(user)
+                db.session.commit()
+
+
+#### SUCCESS TESTS ####
+
+
+# create 2 users for test and print them to output.txt
+def test_create_users(test_client):
+    data_1 = {"id": "10", "first_name": "Terry", "last_name": "Jeffords", "password": "10", "email": "Terry@gmail.com"}
+    data_2 = {"id": "11", "first_name": "Charles", "last_name": "Boyle", "password": "11", "email": "Charles@gmail.com"}
+    response = test_client.post('/users', json=data_1)
+    assert response.status_code == 201
+    print_user_to_file(test_client, 10)
+    response = test_client.post('/users', json=data_2)
+    assert response.status_code == 201
+    print_user_to_file(test_client, 11)
+
+
+# get user details by id
+def test_get_user(test_client):
+    response = test_client.get('/users/10')
+    assert response.status_code == 200
+
+
+# update existing user details
+def test_update_user(test_client):
+    data = {"id": "10", "first_name": "Jake", "last_name": "Peralta", "password": "11", "email": "Jake@gmail.com"}
+    response = test_client.put('/users/10', json=data)
+    assert response.status_code == 200
+    print_user_to_file(test_client, 10)
+
+
+# delete existing user
+def test_delete_user(test_client):
+    response = test_client.delete('/users/10')
+    assert response.status_code == 204
+
+
+#### FAILURE TESTS ####
+
+
+# create user that already exists in DB
+def test_create_existing_user(test_client):
+    data = {"id": "11", "first_name": "Charles", "last_name": "Boyle", "password": "11", "email": "Charles@gmail.com"}
+    response = test_client.put('/users/11', json=data)
+    assert response.status_code == 400
+
+
+# update user that doesn't exist in DB
+def test_update_non_existing_user(test_client):
+    data = {"id": "10", "first_name": "Jake", "last_name": "Peralta", "password": "11", "email": "Jake@gmail.com"}
+    response = test_client.put('/users/10', json=data)
+    assert response.status_code == 400
+
+
+#delete user that doesn't exist in DB
+def test_delete_non_existing_user(test_client):
+    response = test_client.delete('/users/10')
+    assert response.status_code == 400
+
+
+# get user that doesn't exist in DB
+def test_get_non_existing_user(test_client):
+    response = test_client.get('/users/10')
+    assert response.status_code == 400
+
+
+# print created users to output.txt
+def print_user_to_file(test_client, id):
+    response = test_client.get(f'/users/{id}')
+    data = response.get_json()
+    with open('logs/output.log', 'a') as f:
+        if 'Jake' not in str(data):
+            f.write("users created:\n" + str(data) + '\n')
+        else:
+            f.write("users updated:\n" + str(data) + '\n')
